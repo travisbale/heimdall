9046b2026b7eec87b20821d7fc395bd298a33bc2
travis <travis.bale@gmail.com>
2020-10-24 17:45:18 -0500

    Remove Flask-Restful dependency
    Instead use Flask's built in MethodView

--- heimdall/resources.py
+++ heimdall/resources.py
@@ -1,21 +1,18 @@
 """API endpoints for the heimdall service."""
 
-from flask import Blueprint, request
+from flask import Blueprint, jsonify, request
+from flask.views import MethodView
 from flask_jwt_extended.utils import get_jwt_identity
 from flask_jwt_extended.view_decorators import jwt_required
-from flask_restful import Api, Resource
 from marshmallow.exceptions import ValidationError
 from heimdall.models import User, UserSchema
 from http import HTTPStatus
 
 
 bp = Blueprint('api', __name__)
-api = Api(bp)
-
 user_schema = UserSchema()
 
-@api.resource('/users')
-class UsersResource(Resource):
+class UsersResource(MethodView):
     """Application endpoint for User objects."""
 
     def post(self):
@@ -25,16 +22,15 @@ class UsersResource(Resource):
 
             if User.query.filter_by(email=user.email).count() == 0:
                 user.save()
-                return user_schema.dump(user), HTTPStatus.CREATED
+                return jsonify(user_schema.dump(user)), HTTPStatus.CREATED
             else:
-                return {'msg': 'Email has already been registered'}, HTTPStatus.CONFLICT
+                return jsonify({'msg': 'Email has already been registered'}), HTTPStatus.CONFLICT
 
         except ValidationError as e:
-            return e.messages, HTTPStatus.BAD_REQUEST
+            return jsonify(e.messages), HTTPStatus.BAD_REQUEST
 
 
-@api.resource('/users/<int:id>', endpoint='user')
-class UserResource(Resource):
+class UserResource(MethodView):
     """Application endpoint for user objects."""
 
     @jwt_required
@@ -43,6 +39,10 @@ class UserResource(Resource):
 
         if user is not None and user.email == get_jwt_identity():
             user.delete()
-            return {'msg': 'User has been deleted'}, HTTPStatus.OK
+            return jsonify({'msg': 'User has been deleted'}), HTTPStatus.OK
 
-        return {'msg': 'Cannot delete user'}, HTTPStatus.FORBIDDEN
+        return jsonify({'msg': 'Cannot delete user'}), HTTPStatus.FORBIDDEN
+
+
+bp.add_url_rule('/users', view_func=UsersResource.as_view('users_resource'))
+bp.add_url_rule('/users/<int:id>', view_func=UserResource.as_view('user_resource'))
