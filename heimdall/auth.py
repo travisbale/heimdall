e64c847e6af09a60a65577c49c90ab8208699517
travis <travis.bale@gmail.com>
2020-10-24 16:09:54 -0500

    Dump the user schema when user logins successfully
    It will be necessary to send the logged in user's information,
    including their claims, to the client.

--- heimdall/auth.py
+++ heimdall/auth.py
@@ -4,11 +4,13 @@ from flask import Blueprint, request, jsonify
 from flask_jwt_extended import (create_access_token, create_refresh_token,
     set_access_cookies, set_refresh_cookies, jwt_refresh_token_required)
 from flask_jwt_extended.utils import get_jwt_identity, unset_jwt_cookies
-from heimdall.models import User
+from heimdall.models import User, UserSchema
 from http import HTTPStatus
 
 
 bp = Blueprint('auth', __name__)
+
+user_schema = UserSchema()
 
 
 @bp.route('/login', methods=['POST'])
@@ -20,7 +22,7 @@ def login():
     user = User.query.filter_by(email=request.json.get('email')).first()
 
     if user is not None and user.authenticate(request.json.get('password')):
-        response = jsonify({'msg': 'Login successful'})
+        response = jsonify(user_schema.dump(user))
         set_access_cookies(response, create_access_token(identity=user.email))
         set_refresh_cookies(response, create_refresh_token(identity=user.email))
         return response, HTTPStatus.OK
@@ -32,10 +34,15 @@ def login():
 @jwt_refresh_token_required
 def refresh():
     """Issue an authenticated user a new access token."""
-    email = get_jwt_identity()
-    response = jsonify({'msg': 'Refresh successful'})
-    set_access_cookies(response, create_access_token(identity=email))
-    return response, HTTPStatus.OK
+    user = User.query.filter_by(email=get_jwt_identity()).first()
+
+    if user is not None:
+        response = jsonify(user_schema.dump(user))
+        set_access_cookies(response, create_access_token(identity=user.email))
+        return response, HTTPStatus.OK
+
+    # There is no user email associated with that JWT identity
+    return jsonify({'msg': 'Unable to retrieve new access token'}), HTTPStatus.UNAUTHORIZED
 
 
 @bp.route('/logout', methods=['DELETE'])
