0ed41c76b6a94d7877cdb5fec436b9e2f15b5a42
Travis Bale <travis.bale@gmail.com>
2025-11-29 14:22:35 -0600

    Rename library import

--- internal/db/postgres/db.go
+++ internal/db/postgres/db.go
@@ -5,11 +5,11 @@ import (
 
 	"github.com/jackc/pgx/v5"
 	"github.com/travisbale/heimdall/internal/db/postgres/internal/sqlc"
-	db "github.com/travisbale/knowhere/db/postgres"
+	"github.com/travisbale/knowhere/db/postgres"
 )
 
 // DB is the database connection for heimdall
-type DB = db.DB[*sqlc.Queries]
+type DB = postgres.DB[*sqlc.Queries]
 
 // queries wraps sqlc.New to satisfy the db.NewDB constructor signature
 func queries(d any) *sqlc.Queries {
@@ -18,7 +18,7 @@ func queries(d any) *sqlc.Queries {
 
 // NewDB creates a new database connection pool
 func NewDB(ctx context.Context, databaseURL string) (*DB, error) {
-	cfg := db.DefaultConfig()
+	cfg := postgres.DefaultConfig()
 
 	// Load custom types (enums) from database for each connection
 	cfg.AfterConnect = func(ctx context.Context, conn *pgx.Conn) error {
@@ -26,5 +26,5 @@ func NewDB(ctx context.Context, databaseURL string) (*DB, error) {
 		return err
 	}
 
-	return db.NewDB(ctx, databaseURL, queries, cfg)
+	return postgres.NewDB(ctx, databaseURL, queries, cfg)
 }
